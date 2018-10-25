74adbb783605df718172bcf262bcba6ac6af8456
Andy van Sundert <andy482382@gmail.com>
2018-10-25 11:39:37 +0200

    deleted some useless stuff

--- commands/nini.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-	message.channel.send('https://cdn.boob.bot/anal/207E2.jpg')
-	
-	.then(msg => {
-		msg.delete(5000)
-	})
-	message.delete();
-
-} 
--- commands/ronica.js
+++
@@ -1,3 +0,0 @@
-/*exports.run = (client, message, args) => {
-	message.channel.send('RONICA RONICA LAAT ME ZITTEN AAN JE KONT ');
-}
