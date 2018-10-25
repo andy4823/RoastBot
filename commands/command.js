3a4ede1c599a5913f738a200fa3d8a7b5613f9bb
Sem van Elteren <sem407@outlook.com>
2018-10-25 11:30:21 +0200

    added $6

--- commands/command.js
+++ commands/command.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     title: "Mijn commands zijn:",
     fields: [
       { name: "Roasts", value: "$roast\n$offensive ⚠USE AT YOUR OWN RISK⚠"},
-      { name: "Random", value: "$gretavanfleet\n$ping\n$reee\n$roasted\n$slave\n$hellyeah\n$lamp\n$turk\n$marrokaan" },
+      { name: "Random", value: "$gretavanfleet\n$ping\n$reee\n$roasted\n$slave\n$hellyeah\n$lamp\n$turk\n$marrokaan\n$6" },
       { name: "utility", value: "$whatismyavatar\n$say (gebruik: $say (text)"},
       { name: "Admin Commands", value: "$kick\n$ban"}
       ]
