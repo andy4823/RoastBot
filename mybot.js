7c1560c12ee09b8cb5503a556928a5a1da8d0bda
Andy van Sundert <andy482382@gmail.com>
2018-11-01 11:02:51 +0100

    added point system

--- mybot.js
+++ mybot.js
@@ -65,13 +65,7 @@ client.on('guildMemberAdd', (guildMember) => {
    guildMember.addRole(guildMember.guild.roles.find(role => role.name === "monkaS"));
 });
 
-client.on("message", message => {
-  if (message.author.bot) return;
-  if (message.guild) {
-    let score = client.
 
-  }
-});
 
 client.on("ready", () => {
   
@@ -89,5 +83,74 @@ client.on("ready", () => {
   client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
 });
 
+client.on("message", message => {
+  if (message.author.bot) return;
+  let score;
+  if (message.guild) {
+    score = client.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+      message.reply(`Ewa Fakka jij bent een level omhoog je bent nu level **${curLevel}**!   `);
+    }
+    client.setScore.run(score);
+  }
+  if (message.content.indexOf(config.prefix) !== 0) return;
+ 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+  // Command-specific code here!
+    if(command === "points") {
+    return message.reply(`Ewa You currently have ${score.points} points and are level ${score.level}!`);
+  }
+    if(command === "give") {
+  // Limited to guild owner - adjust to your own preference!
+  if(!message.author.id === message.guild.owner) return message.reply("Yain't the boss of me, you can't do that!");
+ 
+  const user = message.mentions.users.first() || client.users.get(args[0]);
+  if(!user) return message.reply("You must mention someone or give their ID!");
+ 
+  const pointsToAdd = parseInt(args[1], 10);
+  if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...")
+ 
+  // Get their current points.
+  let userscore = client.getScore.get(user.id, message.guild.id);
+  // Create data for a user we haven't seen before in the server
+  if (!userscore) {
+    userscore = { id: `${message.guild.id}-${user.id}`, user: user.id, guild: message.guild.id, points: 0, level: 1 }
+  }
+  userscore.points += pointsToAdd;
+ 
+  // We also want to update their level (but we won't (@)notify them if it changes)
+  let userLevel = Math.floor(0.1 * Math.sqrt(score.points));
+  userscore.level = userLevel;
+ 
+  // And we save it!
+  client.setScore.run(userscore);
+ 
+  return message.channel.send(`${user.tag} has received ${pointsToAdd} points and now stands at ${userscore.points} points.`);
+}
+ 
+if(command === "leaderboard") {
+  const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
+ 
+    // Now shake it and show it! (as a nice embed, too!)
+  const embed = new Discord.RichEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setDescription("Our top 10 points leaders!")
+    .setColor("orange");
+ 
+  for(const data of top10) {
+    embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);
+  }
+  return message.channel.send({embed});
+}
 
+});
 client.login(config.token);
