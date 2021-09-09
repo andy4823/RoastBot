f0ea0e1f94faf8454854cfce1a913c8e26d7916b
andy4823 <36826042+andy4823@users.noreply.github.com>
2021-09-09 19:07:13 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
         .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('Ewa kniffo ik ben gejoined ') .catch(console.log);
+          message.reply("I've joined the server") .catch(console.log);
         });
     } else {
       message.reply('You need to join a voice channel first!');
