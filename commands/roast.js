8c09cf29c461d0c5fa6bf0f97e863d2775d3220f
andy4823 <36826042+andy4823@users.noreply.github.com>
2021-09-09 19:11:05 +0200

    Delete roast.js

--- commands/roast.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-
-	function roasten() {
-		var roast = ["ik bel greenpeace voor jou dikke walvis lichaam","jezus man wat een hoofd!", "SO DE TERING","tuberculose slet", "tyfus wees kind"];
-
-		return roast[Math.floor(Math.random()*roast.length)];	
-		message.channel.send(roast).then().catch(console.error);
-	}
-}
